a4d183f37428ae6a65228a440ccdd3b2ae6f35f9
Sylvie-B <unsolo@hotmail.fr>
2021-11-24 21:02:48 +0100

    manipuler du texte

--- exercice1.js
+++ exercice1.js
@@ -1,16 +1,22 @@
 let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
-
+let position = document.getElementById("position");
+let chaineMaj = document.getElementById("chaineMaj");
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
+let result = monTexte.indexOf('i');
 
+position.innerHTML = result.toString();
 
+let upString = monTexte.toUpperCase();
 
+chaineMaj.innerHTML = upString.toString();
 
-
-
-
-
+for (let i = 0 ; i < monTexte.length ; i++){
+    if(monTexte[i] === "a"){
+        alert("lettre a trouvé");
+    }
+}
